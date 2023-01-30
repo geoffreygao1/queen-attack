0e8fb34e1911991fc0163c0ed9ed3c613c8243d1
Geoffrey Gao <geoffreygao1@gmail.com>
2023-01-29 16:12:08 -0800

    Added queen functionality

---
+++ Models/Queen.cs
@@ -0,0 +1,14 @@
+namespace Chess.Models
+{
+  public class Queen
+  {
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public Queen(int x, int y)
+    {
+      X = x;
+      Y = y;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,36 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
+using Chess.Models;
 
-public class Program
+namespace Chess
 {
-  public static void Main()
+  public class Program
   {
+    public static void Main()
+    {
+      Console.WriteLine("Enter X Coordinate of Queen");
+      string stringQueenX = Console.ReadLine();
+      int queenX = int.Parse(stringQueenX);
+      Console.WriteLine("Enter Y Coordinate of Queen");
+      string stringQueenY = Console.ReadLine();
+      int queenY = int.Parse(stringQueenY);
+      Console.WriteLine("Enter X Coordinate of Opponent");
+      string stringOppX = Console.ReadLine();
+      int oppX = int.Parse(stringOppX);
+      Console.WriteLine("Enter Y Coordinate of Opponent");
+      string stringOppY = Console.ReadLine();
+      int oppY = int.Parse(stringOppY);
 
+      Queen yourQueen = new Queen(queenX, queenY);
+      Queen opp = new Queen(oppX, oppY);
+
+      if (yourQueen.X == opp.X || yourQueen.Y == opp.Y || Math.Abs(yourQueen.X - opp.X) == Math.Abs(yourQueen.Y - opp.Y))
+      {
+        Console.WriteLine("You can attack the opposing piece");
+      }
+      else
+      {
+        Console.WriteLine("You can't attack the opposing piece");
+      }
+    }
   }
 }
